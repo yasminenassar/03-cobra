a21834117b61ecb4d9dc6333c783be668ccf077b
Alexander Bakst <abakst@cs.ucsd.edu>
2019-04-29 21:30:16 -0700

    A new start

---
+++ tests/Test.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main where
+
+import Common
+
+import Test.Tasty
+import Paths_cobra
+
+main :: IO ()
+main = do
+  adderTestsFile   <- getDataFileName "tests/adder.json"
+  boaTestsFile     <- getDataFileName "tests/boa.json"
+  cobraTestsFile   <- getDataFileName "tests/cobra.json"
+  anfTestsFile     <- getDataFileName "tests/anf.json"
+  yourTestsFile    <- getDataFileName "tests/yourTests.json"
+
+  anfTests     <- readTests anfTestsFile
+  adderTests   <- readTests adderTestsFile
+  boaTests     <- readTests boaTestsFile
+  cobraTests   <- readTests cobraTestsFile
+  yourTests    <- readTests yourTestsFile
+
+  let tests = testGroup "Tests" $
+                [ testGroup "Normalizer"      anfTests
+                , testGroup "Adder"           adderTests
+                , testGroup "Boa"             boaTests
+                , testGroup "Cobra"           cobraTests
+                , testGroup "Your-Tests"      yourTests
+                ]
+  defaultMain tests
+
+readTests :: FilePath -> IO [TestTree]
+readTests f = map createTestTree <$> parseTestFile f
